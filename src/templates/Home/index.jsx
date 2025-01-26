fa897bce20c4b165e36231ae7b7c8d4a55b4721c
Adão Araujo <adaoma11@hotmail.com>
2025-01-26 16:20:25 -0300

    add a function in Home to fetch page data from api

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react';
 import { Base } from '../Base';
+import { mapPage } from '../../api/mapData';
 import mock from '../Base/mock';
 
 function Home() {
-  return <Base {...mock} />;
+  const [pageData, setPageData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          'http://localhost:1337/api/pages/paywfsq0vd3ecs2j7575ks3y?populate[0]=header.logo.image&populate[1]=header.menu_links&populate[2]=sections.image&populate[3]=sections.images&populate[4]=sections.grid_text&populate[5]=sections.metadata',
+        );
+
+        const json = await response.json();
+        const result = mapPage(json.data);
+        setPageData(result);
+      } catch (err) {
+        setPageData(undefined);
+        console.warn('Ocorreu um erro ao carregar a página: ', err);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return <>{pageData && pageData.slug && <Base {...mock} />}</>;
 }
 
 export default Home;
